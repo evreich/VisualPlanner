085c8e83dc4f10f1dcd7ef24ff904c29fc7c069b
evreich <vlasov_godlike312@mail.ru>
2016-08-04 15:01:04 -0900

    Init project

---
+++ VisualPlanner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisualPlanner.Startup))]
+namespace VisualPlanner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
